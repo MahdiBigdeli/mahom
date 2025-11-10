680c12999221cb72b14559f38dde5bcfc8806ca6
mahdi-vil <jone74243@gmail.com>
2025-11-10 17:28:26 +0330

    mahom3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    $($(".slctor_img_nav")).mouseenter(function () {
+    $($(".slctor_img_nav")[1]).mouseenter(function () {
         $($(".slctor_img_nav")[1]).attr("src","images/text title 2.png")
     })
-     $($(".slctor_img_nav")).mouseout(function () {
+     $($(".slctor_img_nav")[1]).mouseout(function () {
         $($(".slctor_img_nav")[1]).attr("src","images/text title 1 .png")
     })
-    $($(".slctor_img_nav")).mouseenter(function () {
+    $($(".slctor_img_nav")[0]).mouseenter(function () {
         $($(".slctor_img_nav")[0]).attr("src","images/text title 4 .png")
     })
-     $($(".slctor_img_nav")).mouseout(function () {
+     $($(".slctor_img_nav")[0]).mouseout(function () {
         $($(".slctor_img_nav")[0]).attr("src","images/text title 3 .png")
     })
 
